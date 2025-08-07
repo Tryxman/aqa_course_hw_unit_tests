8d04ddb5755bca247ee967fe902f74003306e353
Aliaksei Palianskikh <tryxman11@gmail.com>
2025-08-07 12:25:47 +0200

    Add toLowerCase

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -7,15 +7,15 @@ const word = 'hello';
 const vowelLetters = 'aeoui';
 let vowels = 0;
 let consonants = 0;
+let lettersToLowerCase = word.toLowerCase();
 
 let vowelsAndConsonantsResult = '';
-for (let i = 0; i < word.length; i++) {
-  if (vowelLetters.includes(word[i])) {
+for (let i = 0; i < lettersToLowerCase.length; i++) {
+  if (vowelLetters.includes(lettersToLowerCase[i])) {
     vowels++;
   } else {
     consonants++;
   }
 }
-vowelsAndConsonantsResult = `${word} contains ${vowels} vowels and ${consonants} consonants`;
-
+console.log((vowelsAndConsonantsResult = `${word} contains ${vowels} vowels and ${consonants} consonants`));
 export { vowelsAndConsonantsResult };
